ed4fad619e8e7544e00c6726f49cacba09a3408c
Stas German <stas.german@gmail.com>
2024-08-03 13:09:39 +0300

    add scaffold gem structure

---
+++ lib/summary_action.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "summary_action/version"
+
+module SummaryAction
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/summary_action_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SummaryAction do
+  it "has a version number" do
+    expect(SummaryAction::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
