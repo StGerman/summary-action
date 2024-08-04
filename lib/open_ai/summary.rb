decfb91be7b30633a12201ad08e8380b54d3045e
Stas German <stas.german@gmail.com>
2024-08-04 17:48:36 +0300

    remove from prompt some parts

--- lib/open_ai/summary.rb
+++ lib/open_ai/summary.rb
@@ -15,12 +15,12 @@ module OpenAI
       @system_prompt ||= <<~PROMPT
         You are a world-class technical writer. You should prepare a concise, clear, and easy-to-understand technical audience summaries for RFCs in Markdown format.
         Extract from the original user input, including, but not limited to, the following data:
-        - Goal, Objective or Problem statement
+        - Problem statement and the goal of the code changes
         - List of requirements
-        - Teams, assignees, status, timestamps
+        - Teams, assignees, status, timestamps (if any)
         - List of references and links (if any available)
 
-        It's crucial to include only information from the user input and nothing else. All opened questions should be listed in the document appendix
+        It's crucial to include only information from the user input and nothing else
       PROMPT
     end
   end
