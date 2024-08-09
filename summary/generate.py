a7533f9a8f62a88001aa6ca863b3ff258760429e
Stas German <stas.german@gmail.com>
2024-08-09 21:06:25 +0300

    work on

--- summary/generate.py
+++ summary/generate.py
@@ -8,6 +8,33 @@ Usage: python summary.py <user_input_file>
 import os
 import sys
 import openai # Import the OpenAI library
+import google.generativeai as genai
+
+def gemini_summary(api_key, _user_input, _system_prompt):
+    """
+    Generates a summary using the Gemini API.
+    Args:
+        api_key (str): The OpenAI API key.
+        user_input (str): The user input to summarize.
+        system_prompt (str): The system prompt to use for summarization.
+    Returns:
+        str: The generated summary of the user input.
+    """
+
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    response = model.generate_content(
+        "Tell me a story about a dark magic code",
+        generation_config=genai.types.GenerationConfig(
+            # Only one candidate for now.
+            candidate_count=1,
+            stop_sequences=["x"],
+            max_output_tokens=20,
+            temperature=1.0,
+        ),
+    )
+
+    return response.text
 
 def openai_summary(api_key, user_input, system_prompt):
     """
@@ -55,10 +82,16 @@ def generate_summary(diff_file_path):
     with open("system_prompt.txt", "r", encoding="utf-8") as f:
         system_prompt = f.read()
 
-    api_key = os.environ.get("OPENAI_API_KEY")
+    open_ai_api_key = os.environ.get("OPENAI_API_KEY")
+    gemini_api_key = os.environ.get("GEMINI_API_KEY")
 
-    return openai_summary(api_key, user_input, system_prompt)
+    if open_ai_api_key:
+        return openai_summary(open_ai_api_key, user_input, system_prompt)
 
+    if gemini_api_key:
+        return gemini_summary(gemini_api_key, user_input, system_prompt)
+    else:
+        raise ValueError("No OpenAI or Gemini API key provided.")
 
 if __name__ == "__main__":
     # Ensure the correct number of command-line arguments
